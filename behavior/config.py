593a11dc5991a7c075fc471c6394e25c3a2f539c
Boitumelo45 <b.phetla@icloud.com>
2023-10-04 23:41:17 +0200

    Fixed behavior/config.py

--- behavior/config.py
+++ behavior/config.py
@@ -8,9 +8,14 @@ class Settings(BaseSettings):
     USER_BEHAVIOR_URL: str
     WEEKLY_EXPENSES: str
     MONTHLY_EXPENSES: str
+    PREDICTIVE_EXPENSES: str # this was omitted/extra bonus if you found it
     CATEGORY_EXPENSES: str
     
     class Config:
+        with open('.env', 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                print(line)
         env_file = ".env"
 
 settings = Settings()
